696deee07c99c5731afe44736bbffd878a0aa8f5
Hwangyongjin <dswvgw6412@naver.com>
2023-09-17 00:53:48 +0900

    feat: Task

--- daegyeong/src/pages/MainPage/TaskItem.jsx
+++ daegyeong/src/pages/MainPage/TaskItem.jsx
@@ -19,6 +19,7 @@ const TaskItemDiv = styled.div`
 	& p {
 		text-align: center;
 		color: #bdbdbd;
+		margin-top: 4px;
 	}
 `;
 
@@ -36,7 +37,7 @@ const TaskItem = ({ props }) => {
 				<h3>{props.task}</h3>
 				<p>
 					{props.hashTag.map(ele => {
-						return `#${ele}`;
+						return `#${ele} `;
 					})}
 				</p>
 			</TaskItemElement>
